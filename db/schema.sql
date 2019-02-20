9f3905fb61260b9ab231d26b477cb9893c1c9f7e
David Law <davidsamuellaw@gmail.com>
2019-02-20 13:03:03 -0800

    Collapse db relations and add medias table

--- db/schema.sql
+++ db/schema.sql
@@ -1,20 +1,28 @@
-create table submission_facts (
-    id char(6) primary key,
+create table submissions (
+    id varchar primary key,
     title varchar not null,
     author_fullname varchar not null,
-    url varchar not null unique,
+    author varchar not null,
+    subreddit varchar not null,
+    permalink varchar not null unique,
     created_utc integer not null,
-    search_query varchar not null,
-    date_created datetime default current_timestamp
-);
 
-create table submissions (
-    id char(6) primary key,
-    selftext_html text,
+    selftext_html varchar,
     comments integer not null,
     gilded integer not null,
     downs integer not null,
     ups integer not null,
     score integer not null,
-    foreign key (id) references submission_facts(id)
+
+    search_query varchar not null,
+    date_created datetime default current_timestamp
+);
+
+create table medias (
+    id integer primary key autoincrement,
+    submission_id varchar not null,
+    url varchar not null,
+    is_direct boolean not null,
+    txt varchar,
+    foreign key (submission_id) references submissions(id)
 );
